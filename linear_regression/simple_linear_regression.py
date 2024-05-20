7bcf3bd15e791ab431dfbbb574b79331ca6cc8f3
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2024-05-20 04:10:17 +0330

    Add docString for function

--- linear_regression/simple_linear_regression.py
+++ linear_regression/simple_linear_regression.py
@@ -1,5 +1,53 @@
 import numpy as np
 def slope_and_intercept(x, y):
+    """
+    Calculate the slope and intercept
+
+    Parameters
+    ------
+    x (array_like): 1D array
+    y (array_like): 1D array
+
+    Returns
+    ------
+    slope (float): The line slope in simple linear regression.
+    intercept (float): Constant value in simple linear regression.
+
+    Raises
+    ------
+    ValueError: If the number of elements of x array and y array is equal.
+
+    Exampls
+    ------
+    >>> import numpy as np
+    >>> x = np.array([1, 2, 4 ,6 ,8 ,3, 9])
+    >>> y = np.array([10, 2, 6, 5, 12, 7, 12])
+    >>> slope , intercept = slope_and_intercept(x, y)
+    >>> slope
+    0.6932989690721649
+    >>> intercept
+    4.445876288659794
+
+    ------
+
+    >>> import numpy as np
+    >>> x = np.array([1, 2])
+    >>> y = np.array([3, 4])
+    >>> slope , intercept = slope_and_intercept(x, y)
+    >>> slope
+    1.0
+    >>> intercept
+    2.0
+
+    ------
+    >>> import numpy as np
+    >>> x = np.array([1, 2])
+    >>> y = np.array([3, 4, 7])
+    >>> slope , intercept = slope_and_intercept(x, y)
+    Traceback (most recent call last):
+        raise ValueError("The number of elements of x array and y array must be equal.")
+  
+    """
     if x.ndim != 1:  # Optional check for 1D array
         raise ValueError("x array must be a 1D array.")
     if y.ndim != 1:  # Optional check for 1D array
